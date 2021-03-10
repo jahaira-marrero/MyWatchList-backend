de5c6d23d426fd6fc76940db2cc92d9c92bd0490
Jahaira-Marrero <jahaira.marrero@gmail.com>
2021-03-10 09:58:34 -0500

    UserMovie fixed.

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,37 +1,37 @@
 class UserMoviesController < ApplicationController
     def index
-        @usermovies = Usermovie.order(:user_id)
+        @usermovies = UserMovie.order(:user_id)
         render json: @usermovies
     end
 
     def show
-        @usermovie = Usermovie.find(params[:id])
+        @usermovie = UserMovie.find(params[:id])
         render json: @usermovie
     end
 
     def new
-        @usermovie = Usermovie.new
+        @usermovie = UserMovie.new
         render json: @usermovie
     end
 
     def edit
-        @usermovie = Usermovie.find(params[:id])
+        @usermovie = UserMovie.find(params[:id])
         render json: @usermovie
     end
 
     def create
-        @usermovie = Usermovie.create(usermovie_params)
+        @usermovie = UserMovie.create(usermovie_params)
         render json: @usermovie
     end
 
     def update
-        @usermovie = Usermovie.find(params[:id])
+        @usermovie = UserMovie.find(params[:id])
         @usermovie.update(usermovie_params)
         render json: @usermovie
     end
 
     def destroy
-        @usermovie = Usermovie.find(params[:id])
+        @usermovie = UserMovie.find(params[:id])
         @usermovie.destroy
         render json: @usermovie
     end
